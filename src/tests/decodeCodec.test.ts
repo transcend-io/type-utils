c6d6038ce2052f3847f03e7ec7fc65443ad79a7a
Dipack <dipack@transcend.io>
2024-03-13 16:14:44 -0700

    Add test cases.

---
+++ src/tests/decodeCodec.test.ts
@@ -0,0 +1,44 @@
+import { expect } from 'chai';
+import * as t from 'io-ts';
+import { decodeCodec } from '../codecTools';
+
+describe('decodeCodec', () => {
+  it('should decode a codec as expected', () => {
+    const c = t.type({
+      str: t.string,
+      num: t.number,
+      headers: t.record(t.string, t.string),
+    });
+    const obj = {
+      str: 'abcd',
+      num: 123,
+      headers: {
+        yolo: 'one-life',
+      },
+    };
+    const decoded = decodeCodec(c, obj);
+    expect(decoded).to.deep.equal(obj);
+  });
+
+  it('should throw an error when the input does not match the codec', () => {
+    const c = t.type({
+      str: t.string,
+      num: t.number,
+      headers: t.record(t.string, t.string),
+    });
+    const obj = {
+      str: 123,
+      num: 123,
+      headers: {
+        yolo: 'one-life',
+      },
+    };
+    try {
+      decodeCodec(c, obj);
+    } catch (e) {
+      expect(e.message).to.equal(
+        'Failed to decode codec: [\n  ".str expected type \'string\'"\n]',
+      );
+    }
+  });
+});
