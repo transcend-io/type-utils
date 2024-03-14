60fc12e3141a261e06802a4c50a2ffd4f7faec32
Dipack <dipack@transcend.io>
2024-03-14 10:44:06 -0700

    Log.

--- src/codecTools/decodeCodec.ts
+++ src/codecTools/decodeCodec.ts
@@ -83,7 +83,7 @@ export function decodeCodec<TCodec extends t.Any>(
   }
 
   // FIXME
-  console.log('CATCH ME PLEASE ESLINT');
+  console.log('CATCH ME PLEASE ESLINT1');
 
   // Return the decoded codec
   return decoded.right;
