ace445e812af9d9734d3bb804da8224c1de56ee5
Dipack <dipack@transcend.io>
2024-03-14 10:58:32 -0700

    Skip yarn folder.

--- src/codecTools/decodeCodec.ts
+++ src/codecTools/decodeCodec.ts
@@ -83,7 +83,7 @@ export function decodeCodec<TCodec extends t.Any>(
   }
 
   // FIXME
-  console.log('CATCH ME PLEASE ESLINT4');
+  console.log('CATCH ME PLEASE ESLINT5');
 
   // Return the decoded codec
   return decoded.right;
