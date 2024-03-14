2a872181e3944791840f33077604db09a4079751
Dipack <dipack@transcend.io>
2024-03-14 11:18:23 -0700

    Make a separate pre-commit action.

--- src/codecTools/decodeCodec.ts
+++ src/codecTools/decodeCodec.ts
@@ -83,7 +83,7 @@ export function decodeCodec<TCodec extends t.Any>(
   }
 
   // FIXME
-  console.log('CATCH ME PLEASE ESLINT8');
+  console.log('CATCH ME PLEASE ESLINT9');
 
   // Return the decoded codec
   return decoded.right;
