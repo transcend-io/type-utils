a3d5d4c462b36af898a036415190835683814017
Dipack <dipack@transcend.io>
2024-03-14 10:14:16 -0700

    Re-work the decodeCodec function to return useful information. (#20)
    * Re-work the decodeCodec function to return useful information.
    
    * Re-work the decodeCodec function to return useful information.
    
    * Bump version.
    
    * Add test cases.
    
    * Any.
    
    * Disable the any flag.
    
    * Try to fix the pre-commit
    
    * Remove exclude.

--- src/codecTools/decodeCodec.ts
+++ src/codecTools/decodeCodec.ts
@@ -1,38 +1,46 @@
-// external
-import { fold } from 'fp-ts/lib/Either';
+import { fold, isLeft } from 'fp-ts/lib/Either';
 import { pipe } from 'fp-ts/lib/pipeable';
 import * as t from 'io-ts';
 
-// Determine the paths of the codec that are invalid
-const getPaths = <A>(v: t.Validation<A>): string[] =>
-  pipe(
+/**
+ * Determine the codec paths that are invalid.
+ * @param v - Validation errors.
+ * @returns The error strings formatted for human consumption.
+ */
+function getPaths<A>(v: t.Validation<A>): string[] {
+  return pipe(
     v,
     fold(
       (errors) =>
         errors.map((error) => {
-          const lastError = error.context[error.context.length - 1];
-          return `${error.context
-            .map((context) => context.key)
-            // Note: lastError.actual is helpful for debugging
-            .join('.')} expected type '${lastError.type.name}'`;
+          const lastCtx = error.context.at(-1);
+          const fullPath = error.context.map(({ key }) => key).join('.');
+          return `${fullPath} expected type '${lastCtx?.type.name}'`;
         }),
 
       () => ['no errors'],
     ),
   );
+}
 
-// Get custom error messages for the errors
-const getCustomErrors = <A>(
+/**
+ * Get custom error messages for the errors.
+ * @param v - Validation errors.
+ * @param customErrorFromContext - Custom mapper function.
+ * @returns The custom error strings formatted for human consumption.
+ */
+function getCustomErrors<A>(
   v: t.Validation<A>,
   customErrorFromContext: (validationContext: t.Context) => string,
-): string[] =>
-  pipe(
+): string[] {
+  return pipe(
     v,
     fold(
       (errors) => errors.map((error) => customErrorFromContext(error.context)),
       () => ['no errors'],
     ),
   );
+}
 
 export const CODEC_ERROR_MESSAGE = 'Failed to decode codec:';
 
@@ -48,26 +56,29 @@ export function decodeCodec<TCodec extends t.Any>(
   codec: TCodec,
   txt: string | object | object[] | null | unknown,
   parse = true,
-  // Warning: DO NOT LOG SENSITIVE DATA WITH THIS!
-  customErrorFromContext: (validationContext: t.Context) => string = () => '',
+  customErrorFromContext:
+    | ((validationContext: t.Context) => string)
+    | undefined = undefined,
 ): t.TypeOf<TCodec> {
   // uncomment the below to view the response pre-decode
   const decoded = codec.decode(
     parse && typeof txt === 'string' ? JSON.parse(txt) : txt,
   );
   // Log errors on failure
-  // eslint-disable-next-line no-underscore-dangle
-  if (decoded._tag === 'Left') {
+  if (isLeft(decoded)) {
+    const errorPaths = getPaths(decoded);
+    const customError =
+      customErrorFromContext !== undefined
+        ? JSON.stringify(
+            getCustomErrors(decoded, customErrorFromContext),
+            null,
+            2,
+          )
+        : undefined;
     throw new Error(
-      `${CODEC_ERROR_MESSAGE} \n\n${JSON.stringify(
-        getPaths(decoded),
-        null,
-        2,
-      )}${JSON.stringify(
-        getCustomErrors(decoded, customErrorFromContext),
-        null,
-        2,
-      )}`,
+      `${CODEC_ERROR_MESSAGE} ${JSON.stringify(errorPaths, null, 2)}${
+        customError ?? ''
+      }`,
     );
   }
 
---
+++ src/tests/decodeCodec.test.ts
@@ -0,0 +1,45 @@
+import { expect } from 'chai';
+import * as t from 'io-ts';
+import { decodeCodec } from '../codecTools';
+
+describe('decodeCodec', () => {
+  it('should decode a codec as expected', () => {
+    const c = t.type({
+      str: t.string,
+      num: t.number,
+      headers: t.record(t.string, t.string),
+    });
+    const obj = {
+      str: 'abcd',
+      num: 123,
+      headers: {
+        yolo: 'one-life',
+      },
+    };
+    const decoded = decodeCodec(c, obj);
+    expect(decoded).to.deep.equal(obj);
+  });
+
+  it('should throw an error when the input does not match the codec', () => {
+    const c = t.type({
+      str: t.string,
+      num: t.number,
+      headers: t.record(t.string, t.string),
+    });
+    const obj = {
+      str: 123,
+      num: 123,
+      headers: {
+        yolo: 'one-life',
+      },
+    };
+    try {
+      decodeCodec(c, obj);
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    } catch (e: any) {
+      expect(e.message).to.equal(
+        'Failed to decode codec: [\n  ".str expected type \'string\'"\n]',
+      );
+    }
+  });
+});
