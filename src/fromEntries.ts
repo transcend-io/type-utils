472139b3eb3c8926419c972404cd99ba993b2b26
Cami <50341430+csmccarthy@users.noreply.github.com>
2025-10-14 15:01:13 -0700

    Csmccarthy/from entries (#38)
    * type safe from entries
    
    * jsdoc
    
    * lint
    
    * lint

--- src/fromEntries.ts
+++ src/fromEntries.ts
@@ -5,8 +5,6 @@
  */
 export function fromEntries<
   const T extends ReadonlyArray<readonly [PropertyKey, unknown]>,
->(
-  entries: T,
-): { [K in T[number] as K[0]]: K[1] } {
+>(entries: T): { [K in T[number] as K[0]]: K[1] } {
   return Object.fromEntries(entries) as { [K in T[number] as K[0]]: K[1] };
 }
