8c46ea25b407902401e38ed1dd55112f0cbdfb7e
Cami <50341430+csmccarthy@users.noreply.github.com>
2025-10-14 14:35:20 -0700

    type safe from entries (#37)
    * type safe from entries
    
    * jsdoc
    
    * lint

---
+++ src/fromEntries.ts
@@ -0,0 +1,12 @@
+/**
+ * Object.fromEntries that actually preserves entries as types.
+ * @param entries - The entries to build the object from
+ * @returns The object as built from the entries
+ */
+export function fromEntries<
+  const T extends ReadonlyArray<readonly [PropertyKey, unknown]>,
+>(
+  entries: T,
+): { [K in T[number] as K[0]]: K[1] } {
+  return Object.fromEntries(entries) as { [K in T[number] as K[0]]: K[1] };
+}
